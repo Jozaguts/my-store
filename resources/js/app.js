d4e793eef7b15b3fcc25b0868c5f7220fdf2b700
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-06-25 17:29:03 -0500

    add vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+
+Vue.component('example-component', require('./components/ExampleComponent').default);
+
+const app = new Vue({
+    el: '#app'
+})
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>{{data}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            data: 'Funciona'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
