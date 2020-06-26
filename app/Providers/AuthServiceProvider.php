2ddf084892fe8faa58b95f526b6fe016b01e7b77
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-06-25 21:55:56 -0500

    laravel passport installed

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,7 +24,14 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        // just client grant password route
+        Passport::routes(function ($router) {
+            $router->forAccessTokens();
+        });
+        Passport::tokensExpireIn(now()->addDays(15));
 
-        //
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 }
