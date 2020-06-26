41bc706c66d744408f491946cdc9fa8f7e028905
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-06-26 11:55:33 -0500

    add SoftDelete into user model

--- app/User.php
+++ app/User.php
@@ -3,13 +3,14 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable, HasApiTokens;
+    use Notifiable, HasApiTokens, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
