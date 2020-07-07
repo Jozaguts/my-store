2d2044ed01af47a6cbd2cd033e576ab32d804619
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-07-07 10:15:48 -0500

    Form requests create &  update for producController, create, & update method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateProduct;
+use App\Http\Requests\UpdateProduct;
 use Illuminate\Http\Request;
 use App\Product;
 
@@ -9,9 +11,9 @@ class ProductController extends Controller
 {
     public function index(Request $request)
     {
-        
+
         try {
-            $products = Product::paginate(9, ['id', 'name', 'slug', 'description', 'price', 'status'],'page',$request->page);
+            $products = Product::paginate(9, ['id', 'name', 'slug', 'description', 'price', 'status'], 'page', $request->page);
             return response()->json(
                 [
                     'paginate' => [
@@ -30,4 +32,60 @@ class ProductController extends Controller
             return response()->json(['error', $th->getMessage()], 400);
         }
     }
+    public function sendFirstPage()
+    {
+        try {
+            $products = Product::paginate(9, ['id', 'name', 'slug', 'description', 'price', 'status'], 'page', '1');
+            return response()->json(
+                [
+                    'paginate' => [
+                        'total' => $products->total(),
+                        'current_page' => $products->currentPage(),
+                        'per_page' => $products->perPage(),
+                        'last_page' => $products->lastPage(),
+                        'from' => $products->firstItem(),
+                        'to' => $products->lastPage(),
+                    ],
+                    'products' => $products
+                ],
+                200
+            );
+        } catch (\Throwable $th) {
+            return response()->json(['error', $th->getMessage()], 400);
+        }
+    }
+    public function create(CreateProduct $request)
+    {
+        try {
+            Product::create($request->all())->save();
+            return $this->sendFirstPage();
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th->getMessage()], 400);
+        }
+    }
+    public function update(UpdateProduct $request)
+    {
+        try {
+            $product = Product::find($request->id);
+          
+            if ($product->slug === $request->slug) {
+                $product->update($request->except(['id', 'slug']));
+            } else {
+                $product->update($request->except(['id']));
+            }
+            return $this->sendFirstPage();
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th->getMessage()], 400);
+        }
+    }
+    // public function delete($id)
+    // {
+    //     try {
+    //         $user = User::find($id);
+    //         $user->delete();
+    //         return $this->index();
+    //     } catch (\Throwable $th) {
+    //         return response()->json(['errors' => $th->getMessage()], 400);
+    //     }
+    // }
 }
